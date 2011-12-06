520bc01037f7b49ea16075b7eb6bc25685953a83
Peter Kelly <peter@uxproductivity.com>
2011-12-06 23:26:31 +0000

    In the Position and Range classes, use a single assignment statement for each method, to be consistent with what we've used elsewhere.

--- src/Position.js
+++ src/Position.js
@@ -5,49 +5,52 @@ function Position(node,offset)
     this.origOffset = offset;
 }
 
-Position.prototype = {
-    moveToStartOfWord: function() {
-        var text = this.node.nodeValue;
-        this.offset = this.origOffset;
-        while ((this.offset > 0) && isWordChar(text.charAt(this.offset-1)))
-            this.offset--;
-    },
+Position.prototype.moveToStartOfWord = function()
+{
+    var text = this.node.nodeValue;
+    this.offset = this.origOffset;
+    while ((this.offset > 0) && isWordChar(text.charAt(this.offset-1)))
+        this.offset--;
+}
 
-    moveToEndOfWord: function() {
-        var text = this.node.nodeValue;
-        var length = text.length;
-        this.offset = this.origOffset;
-        while ((this.offset < length) && isWordChar(text.charAt(this.offset)))
-            this.offset++;
-    },
+Position.prototype.moveToEndOfWord = function()
+{
+    var text = this.node.nodeValue;
+    var length = text.length;
+    this.offset = this.origOffset;
+    while ((this.offset < length) && isWordChar(text.charAt(this.offset)))
+        this.offset++;
+}
 
-    moveForwardIfAtEnd: function() {
-        if ((this.node.nodeType == Node.TEXT_NODE) &&
-            (this.offset == this.node.nodeValue.length)) {
-            var next = nextTextNode(this.node);
-            if (next != null) {
-                this.node = next;
-                this.offset = 0;
-                // debug("Moved start to "+this.toString()+"\n");
-            }
+Position.prototype.moveForwardIfAtEnd = function()
+{
+    if ((this.node.nodeType == Node.TEXT_NODE) &&
+        (this.offset == this.node.nodeValue.length)) {
+        var next = nextTextNode(this.node);
+        if (next != null) {
+            this.node = next;
+            this.offset = 0;
+            // debug("Moved start to "+this.toString()+"\n");
         }
-    },
-
-    moveBackwardIfAtStart: function() {
-        if ((this.node.nodeType == Node.TEXT_NODE) &&
-            (this.offset == 0)) {
-            var prev = prevTextNode(this.node);
-            if (prev != null) {
-                this.node = prev;
-                this.offset = this.node.nodeValue.length;
-                // debug("Moved end to "+this.toString()+"\n");
-            }
-        }
-    },
-
-    toString: function() {
-        return this.node.nodeName+" \""+
-               this.node.nodeValue+"\" offset "+
-               this.offset;
     }
-};
+}
+
+Position.prototype.moveBackwardIfAtStart = function()
+{
+    if ((this.node.nodeType == Node.TEXT_NODE) &&
+        (this.offset == 0)) {
+        var prev = prevTextNode(this.node);
+        if (prev != null) {
+            this.node = prev;
+            this.offset = this.node.nodeValue.length;
+            // debug("Moved end to "+this.toString()+"\n");
+        }
+    }
+}
+
+Position.prototype.toString = function()
+{
+    return this.node.nodeName+" \""+
+    this.node.nodeValue+"\" offset "+
+    this.offset;
+}
--- src/Range.js
+++ src/Range.js
@@ -17,122 +17,120 @@ Range.fromSelection = function()
                      new Position(r.endContainer,r.endOffset));
 }
 
-Range.prototype = {
-    selectWholeWords: function()
-    {
-        if ((this.start.node.nodeType == Node.TEXT_NODE) &&
-            (this.end.node.nodeType == Node.TEXT_NODE)) {
-            if (this.isForwards()) {
-                // this.start comes before this.end
-                this.start.moveToStartOfWord();
-                this.end.moveToEndOfWord();
-            }
-            else {
-                // this.end comes before this.end
-                this.start.moveToEndOfWord();
-                this.end.moveToStartOfWord();
-            }
+Range.prototype.selectWholeWords = function()
+{
+    if ((this.start.node.nodeType == Node.TEXT_NODE) &&
+        (this.end.node.nodeType == Node.TEXT_NODE)) {
+        if (this.isForwards()) {
+            // this.start comes before this.end
+            this.start.moveToStartOfWord();
+            this.end.moveToEndOfWord();
         }
-    },
-
-    omitEmptyTextSelection: function()
-    {
-        this.start.moveForwardIfAtEnd();
-        this.end.moveBackwardIfAtStart();
-    },
-
-    isForwards: function()
-    {
-        if (this.start.node == this.end.node)
-            return (this.start.offset < this.end.offset);
-        else
-            return (this.start.node.compareDocumentPosition(this.end.node) ==
-                    Node.DOCUMENT_POSITION_FOLLOWING);
-    },
-
-    getParagraphNodes: function()
-    {
-        var result = new Array();
-        var node = this.start.node;
-        while (!isParagraphNode(node))
-            node = node.parentNode;
-        while (true) {
-            if (isParagraphNode(node))
-                result.push(node);
-            if (node == this.end.node)
-                break;
-            node = nextNode(node);
+        else {
+            // this.end comes before this.end
+            this.start.moveToEndOfWord();
+            this.end.moveToStartOfWord();
         }
-        return result;
-    },
+    }
+}
 
-    getInlineNodes: function()
-    {
-        var result = new Array();
-        var node = this.start.node;
-        while (true) {
-            if (isInlineNode(node)) {
-                debug("getInlineNodes: "+node.nodeName+" is inline");
-                result.push(node);
-            }
-            else {
-                debug("getInlineNodes: "+node.nodeName+" is NOT inline");
-            }
-            if (node == this.end.node)
-                break;
-            node = nextNode(node);
-        }
-        return result;
-    },
+Range.prototype.omitEmptyTextSelection = function()
+{
+    this.start.moveForwardIfAtEnd();
+    this.end.moveBackwardIfAtStart();
+}
 
-    getAllNodes: function()
-    {
-        var result = new Array();
-        var node = this.start.node;
-        while (true) {
+Range.prototype.isForwards = function()
+{
+    if (this.start.node == this.end.node)
+        return (this.start.offset < this.end.offset);
+    else
+        return (this.start.node.compareDocumentPosition(this.end.node) ==
+                Node.DOCUMENT_POSITION_FOLLOWING);
+}
+
+Range.prototype.getParagraphNodes = function()
+{
+    var result = new Array();
+    var node = this.start.node;
+    while (!isParagraphNode(node))
+        node = node.parentNode;
+    while (true) {
+        if (isParagraphNode(node))
             result.push(node);
-            if (node == this.end.node)
-                break;
-            node = nextNode(node);
+        if (node == this.end.node)
+            break;
+        node = nextNode(node);
+    }
+    return result;
+}
+
+Range.prototype.getInlineNodes = function()
+{
+    var result = new Array();
+    var node = this.start.node;
+    while (true) {
+        if (isInlineNode(node)) {
+            debug("getInlineNodes: "+node.nodeName+" is inline");
+            result.push(node);
         }
-        return result;
-    },
-
-    setSelection: function()
-    {
-        window.getSelection().setBaseAndExtent(this.start.node,
-                                               this.start.offset,
-                                               this.end.node,
-                                               this.end.offset);
-    },
-
-    ensureRangeValidHierarchy: function()
-    {
-        var nodes = this.getAllNodes();
-
-        var depths = new Array();
-        for (var i = 0; i < nodes.length; i++) {
-            var depth = getNodeDepth(nodes[i]);
-            if (depths[depth] == null) {
-                depths[depth] = new Array();
-            }
-            depths[depth].push(nodes[i]);
+        else {
+            debug("getInlineNodes: "+node.nodeName+" is NOT inline");
         }
+        if (node == this.end.node)
+            break;
+        node = nextNode(node);
+    }
+    return result;
+}
 
-        for (var depth = 0; depth < depths.length; depth++) {
-            var firstDepth = true;
-            if (depths[depth] != null) {
-                for (var i = 0; i < depths[depth].length; i++) {
-                    var node = depths[depth][i];
-                    if (!isInlineNode(node.parentNode) && isWhitespaceTextNode(node)) {
-                        node.parentNode.removeChild(node);
-                    }
-                    else {
-                        ensureValidHierarchy(node,firstDepth);
-                    }
+Range.prototype.getAllNodes = function()
+{
+    var result = new Array();
+    var node = this.start.node;
+    while (true) {
+        result.push(node);
+        if (node == this.end.node)
+            break;
+        node = nextNode(node);
+    }
+    return result;
+}
+
+Range.prototype.setSelection = function()
+{
+    window.getSelection().setBaseAndExtent(this.start.node,
+                                           this.start.offset,
+                                           this.end.node,
+                                           this.end.offset);
+}
+
+Range.prototype.ensureRangeValidHierarchy = function()
+{
+    var nodes = this.getAllNodes();
+    
+    var depths = new Array();
+    for (var i = 0; i < nodes.length; i++) {
+        var depth = getNodeDepth(nodes[i]);
+        if (depths[depth] == null) {
+            depths[depth] = new Array();
+        }
+        depths[depth].push(nodes[i]);
+    }
+    
+    for (var depth = 0; depth < depths.length; depth++) {
+        var firstDepth = true;
+        if (depths[depth] != null) {
+            for (var i = 0; i < depths[depth].length; i++) {
+                var node = depths[depth][i];
+                if (!isInlineNode(node.parentNode) && isWhitespaceTextNode(node)) {
+                    node.parentNode.removeChild(node);
                 }
-                firstDepth = false;
+                else {
+                    ensureValidHierarchy(node,firstDepth);
+                }
             }
+            firstDepth = false;
         }
     }
 }
