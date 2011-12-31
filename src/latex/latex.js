82e3386748e97f2f523116c013a8d53a20913759
Peter Kelly <peter@uxproductivity.com>
2011-12-31 03:05:06 +0000

    More progress on LaTeX parsing - parse the tokens using a hand-constructed grammar into a syntax tree.

--- src/latex/latex.js
+++ src/latex/latex.js
@@ -1,3 +1,10 @@
+function extend(base)
+{
+    function inheritance() {}
+    inheritance.prototype = base.prototype;
+    return new inheritance();
+}
+
 function load()
 {
     req = new XMLHttpRequest("http://localhost");
@@ -14,6 +21,183 @@ function save()
     alert("req = "+req.readyState);
 }
 
+////////////////////////////////////////////////////////////////////////////////////////////////////
+//                                                                                                //
+//                                         LatexNode                                              //
+//                                                                                                //
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+function LatexNode()
+{
+    this.firstChild = null;
+    this.lastChild = null;
+    this.parentNode = null;
+    this.previousSibling = null;
+    this.nextSibling = null;
+}
+
+LatexNode.prototype.appendChild = function(child)
+{
+    if (this.firstChild == null) {
+        this.firstChild = child;
+        this.lastChild = child;
+    }
+    else {
+        this.lastChild.nextSibling == child;
+        child.previousSibling == this.lastChild;
+        this.lastChild = child;
+    }
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+//                                                                                                //
+//                                        LatexElement                                            //
+//                                                                                                //
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+LatexElement.prototype = extend(LatexNode);
+
+function LatexElement()
+{
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+//                                                                                                //
+//                                        LatexComment                                            //
+//                                                                                                //
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+LatexComment.prototype = extend(LatexNode);
+
+function LatexComment(text)
+{
+    this.text = text;
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+//                                                                                                //
+//                                          LatexText                                             //
+//                                                                                                //
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+LatexText.prototype = extend(LatexNode);
+
+function LatexText(text)
+{
+    this.text = text;
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+//                                                                                                //
+//                                         LatexArgument                                          //
+//                                                                                                //
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+LatexArgument.prototype = extend(LatexNode);
+
+function LatexArgument(type,children)
+{
+    this.type = type;
+    for (var i = 0; i < children.length; i++)
+        this.appendChild(children[i]);
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+//                                                                                                //
+//                                          LatexArgRef                                           //
+//                                                                                                //
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+LatexArgRef.prototype = extend(LatexNode);
+
+function LatexArgRef(num)
+{
+    this.num = num;
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+//                                                                                                //
+//                                          LatexMacro                                            //
+//                                                                                                //
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+LatexMacro.prototype = extend(LatexNode);
+
+function LatexMacro(name,args)
+{
+    this.name = name;
+    for (var i = 0; i < args.length; i++)
+        this.appendChild(args[i]);
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+//                                                                                                //
+//                                           LatexMath                                            //
+//                                                                                                //
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+LatexMath.prototype = extend(LatexNode);
+
+function LatexMath(children)
+{
+    for (var i = 0; i < children.length; i++)
+        this.appendChild(children[i]);
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+//                                                                                                //
+//                                           LatexNbsp                                            //
+//                                                                                                //
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+LatexNbsp.prototype = extend(LatexNode);
+
+function LatexNbsp()
+{
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+//                                                                                                //
+//                                         LatexSuperscript                                       //
+//                                                                                                //
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+LatexSuperscript.prototype = extend(LatexNode);
+
+function LatexSuperscript()
+{
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+//                                                                                                //
+//                                          LatexSubscript                                        //
+//                                                                                                //
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+LatexSubscript.prototype = extend(LatexNode);
+
+function LatexSubscript()
+{
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+//                                                                                                //
+//                                          LatexAmpersand                                        //
+//                                                                                                //
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+LatexAmpersand.prototype = extend(LatexNode);
+
+function LatexAmpersand()
+{
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+//                                                                                                //
+//                                          LatexToken                                            //
+//                                                                                                //
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
 function LatexToken(type,offset,length)
 {
     this.type = type;
@@ -21,14 +205,24 @@ function LatexToken(type,offset,length)
     this.length = length;
 }
 
-LatexToken.prototype.TEXT = 0;
-LatexToken.prototype.COMMENT = 0;
+LatexToken.END = 0;
+LatexToken.TEXT = 1;
+LatexToken.COMMENT = 2;
+LatexToken.MACRO = 3;
+LatexToken.ARG_REF = 4;
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+//                                                                                                //
+//                                         LatexSource                                            //
+//                                                                                                //
+////////////////////////////////////////////////////////////////////////////////////////////////////
 
 function LatexSource(code)
 {
     this.pos = -1;
     this.code = code;
     this.char = this.nextChar();
+    this.token = this.nextToken();
 }
 
 LatexSource.prototype.specialChars = {
@@ -39,7 +233,7 @@ LatexSource.prototype.specialChars = {
     "~": true,
     "_": true,
     "^": true,
-    "\"": true,
+    "\\": true,
     "{": true,
     "}": true,
     "[": true,
@@ -55,6 +249,16 @@ LatexSource.prototype.isMacroChar = function(c)
     return (((c >= 'a') && (c <= 'z')) ||
             ((c >= 'A') && (c <= 'Z')) ||
             ((c >= '0') && (c <= '9')));
+//    var charCode = c.charCodeAt(0);
+//    return (((c >= 97) && (c <= 122)) ||
+//            ((c >= 65) && (c <= 90)) ||
+//            ((c >= 48) && (c <= 57)));
+    return false;
+}
+
+LatexSource.prototype.isDigit = function(c)
+{
+    return ((c >= '0') && (c <= '9'));
 }
 
 LatexSource.prototype.nextChar = function()
@@ -80,51 +284,167 @@ LatexSource.prototype.matchChar = function(c)
     }
 }
 
-LatexSource.prototype.next = function()
+LatexSource.prototype.nextToken = function()
 {
-    if (this.char == null)
-        return null;
+    if (this.char == null) {
+        this.token = null;
+        return this.token;
+    }
     
     var start = this.pos;
     if (this.char == '%') {
         while ((this.char != null) && (this.char != '\n'))
             this.nextChar();
-        var token = new LatexToken(LatexToken.COMMENT,start,this.pos-start);
-        var tokenText = this.code.slice(token.offset,token.offset+token.length);
+        this.token = new LatexToken(LatexToken.COMMENT,start,this.pos-start);
         if (this.char == '\n')
             this.nextChar();
-        return token;
+    }
+    else if (this.char == '\\') {
+        this.nextChar();
+        // FIXME: is whitespace allowed before/after the macro name?
+        while ((this.char != null) && this.isMacroChar(this.char))
+            this.nextChar();
+        this.token = new LatexToken(LatexToken.MACRO,start+1,this.pos-start-1);
+    }
+    else if (this.char == '#') {
+        this.nextChar();
+        while ((this.char != null) && this.isDigit(this.char))
+            this.nextChar();
+        this.token = new LatexToken(LatexToken.ARG_REF,start+1,this.pos-start-1);
     }
     else if (this.isSpecialChar(this.char)) {
+        this.token = new LatexToken(this.char,start,1);
         this.nextChar();
-        return new LatexToken(this.char,start,1);
     }
     else {
+        // FIXME: handle characters quoted with backslash
         while ((this.char != null) && !this.isSpecialChar(this.char))
             this.nextChar();
-        return new LatexToken(LatexToken.TEXT,start,this.pos-start);
+        this.token = new LatexToken(LatexToken.TEXT,start,this.pos-start);
     }
+    return this.token;
+}
+
+LatexSource.prototype.parseSingle = function(indent)
+{
+    if (this.token == null)
+        return null;
+
+    var tokenText = this.code.slice(this.token.offset,this.token.offset+this.token.length);
+    if (this.token.type == LatexToken.COMMENT) {
+        this.nextToken();
+        debug(indent+"Comment: "+tokenText);
+        return new LatexComment(tokenText);
+    }
+    else if (this.token.type == LatexToken.TEXT) {
+        this.nextToken();
+        debug(indent+"Text: "+tokenText);
+        return new LatexText(tokenText);
+    }
+    else if (this.token.type == LatexToken.MACRO) {
+        this.nextToken();
+        debug(indent+"Macro: "+tokenText);
+        var name = tokenText;
+        var args = new Array();
+        while ((this.token != null) && ((this.token.type == '[') || (this.token.type == '{')))
+            args.push(this.parseSingle(indent+"    "));
+        return new LatexMacro(name,args);
+    }
+    else if (this.token.type == '[') {
+        this.nextToken();
+        debug(indent+"[] arg");
+        var type = '[';
+        var children = new Array();
+        while ((this.token != null) && (this.token.type != ']'))
+            children.push(this.parseSingle(indent+"    "));
+        this.nextToken();
+        return new LatexArgument(type,children);
+    }
+    else if (this.token.type == '{') {
+        this.nextToken();
+        debug(indent+"{} arg");
+        var type = '{';
+        var children = new Array();
+        while ((this.token != null) && (this.token.type != '}'))
+            children.push(this.parseSingle(indent+"    "));
+        this.nextToken();
+        return new LatexArgument(type,children);
+    }
+    else if (this.token.type == LatexToken.ARG_REF) {
+        this.nextToken();
+        debug(indent+"arg ref "+tokenText);
+        return new LatexArgRef(parseInt(tokenText));
+    }
+    else if (this.token.type == '$') {
+        this.nextToken();
+        debug(indent+"math mode");
+        var children = new Array();
+        while ((this.token != null) && (this.token.type != '$'))
+            children.push(this.parseSingle(indent+"    "));
+        this.nextToken();
+        return new LatexMath(children);
+    }
+    else if (this.token.type == '~') {
+        this.nextToken();
+        debug(indent+"Non-breaking space");
+        return new LatexNbsp();
+    }
+    else if (this.token.type == '^') {
+        this.nextToken();
+        debug(indent+"Superscript");
+        return new LatexSuperscript();
+    }
+    else if (this.token.type == '_') {
+        this.nextToken();
+        debug(indent+"Subscript");
+        return new LatexSubscript();
+    }
+    else if (this.token.type == '&') {
+        this.nextToken();
+        debug(indent+"Ampersand");
+        return new LatexAmpersand();
+    }
+    else {
+        throw new Error("Parse error: Unexpected token "+this.token.type);
+    }
+}
+
+LatexSource.prototype.parseDocument = function()
+{
+    var root = new LatexElement();
+    var element;
+    while ((element = this.parseSingle("")) != null)
+        root.appendChild(element);
+    return root;
 }
 
 LatexSource.prototype.parse = function()
 {
-    var token;
-    while ((token = this.next()) != null) {
-        var tokenText = this.code.slice(token.offset,token.offset+token.length);
-//        var tokenText = this.code.slice(0,token.length);
-//        debug("start = "+token.offset+", end = "+(token.offset+token.length));
-        if (token.type == LatexToken.COMMENT) {
-            debug("Comment: \""+tokenText+"\"");
-        }
-        else if (token.type == LatexToken.TEXT) {
-            debug("Text: \""+tokenText+"\"");
-        }
-        else {
-            debug("Character: "+tokenText);
-        }
-    }
-    debug("token = null");
+//    var token;
+//    for (; this.token != null; this.nextToken()) {
+//        var tokenText = this.code.slice(this.token.offset,this.token.offset+this.token.length);
+//        if (this.token.type == LatexToken.COMMENT) {
+//            debug("Comment: \""+tokenText+"\"");
+//        }
+//        else if (this.token.type == LatexToken.TEXT) {
+//            debug("Text: \""+tokenText+"\"");
+//        }
+//        else if (this.token.type == LatexToken.MACRO) {
+//            debug("Macro: "+tokenText+"");
+//        }
+//        else {
+//            debug("Character: "+tokenText);
+//        }
+//    }
+//    debug("token = null");
+    this.parseDocument();
 }
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+//                                                                                                //
+//                                            main                                                //
+//                                                                                                //
+////////////////////////////////////////////////////////////////////////////////////////////////////
 
 function main()
 {
