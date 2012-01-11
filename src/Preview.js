7adcf29a28fcae4dd32b45802f8936e5057606fa
Peter Kelly <peter@uxproductivity.com>
2012-01-11 18:19:21 +0000

    Added manual line-wrapping to the sample text. This doesn't affect the display for normal HTML elements, but for PRE the explicit line breaks are used when laying out the page. This prevents all the text from appearing on a single line when the built-in PRE ("Preformatted text") style is selected, and instead makes it clearer that the text will wrap as-is, rather than fitting to the width of the page.

--- src/Preview.js
+++ src/Preview.js
@@ -1,12 +1,22 @@
 function setStyleSheet(selector,cssText)
 {
-    var previewText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec in diam mauris. Integer in lorem sit amet dolor lacinia aliquet. Cras vehicula odio non enim euismod nec congue lorem varius. Sed eu libero arcu, eget tempus augue. Vivamus varius risus ac libero sagittis eu ultricies lectus consequat. Integer gravida accumsan fermentum. Morbi erat ligula, volutpat non accumsan sed, pellentesque quis purus. Vestibulum vestibulum tincidunt lectus non pellentesque. Quisque porttitor sollicitudin tellus, id porta velit interdum sit amet. Cras quis sem orci, vel convallis magna. Pellentesque congue, libero et iaculis volutpat, enim turpis sodales dui, lobortis pharetra lectus dolor at sem. Nullam aliquam, odio ac laoreet vulputate, ligula nunc euismod leo, vel bibendum magna leo ut orci. In tortor turpis, pellentesque nec cursus ut, consequat non ipsum. Praesent venenatis, leo in pulvinar pharetra, eros nisi convallis elit, vitae luctus magna velit ut lorem."
+    var previewText =
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec in diam \n"+
+    "mauris. Integer in lorem sit amet dolor lacinia aliquet. Cras vehicula odio \n"+
+    "non enim euismod nec congue lorem varius. Sed eu libero arcu, eget tempus \n"+
+    "augue. Vivamus varius risus ac libero sagittis eu ultricies lectus \n"+
+    "consequat. Integer gravida accumsan fermentum. Morbi erat ligula, volutpat \n"+
+    "non accumsan sed, pellentesque quis purus. Vestibulum vestibulum tincidunt \n"+
+    "lectus non pellentesque. Quisque porttitor sollicitudin tellus, id porta \n"+
+    "velit interdum sit amet. Cras quis sem orci, vel convallis magna. \n"+
+    "Pellentesque congue, libero et iaculis volutpat, enim turpis sodales dui, \n"+
+    "lobortis pharetra lectus dolor at sem. Nullam aliquam, odio ac laoreet \n"+
+    "vulputate, ligula nunc euismod leo, vel bibendum magna leo ut orci. In \n"+
+    "tortor turpis, pellentesque nec cursus ut, consequat non ipsum. Praesent \n"+
+    "venenatis, leo in pulvinar pharetra, eros nisi convallis elit, vitae luctus \n"+
+    "magna velit ut lorem."
 
     var style = document.getElementById("style");
-//    alert("js: setStyleSheet");
-//    alert("style = "+style);
-//    alert("selector = "+selector);
-//    alert("cssText = "+cssText);
 
     while (style.firstChild != null)
         style.removeChild(style.firstChild);
