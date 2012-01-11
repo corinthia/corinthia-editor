3e3cd1ea139e86fa63395aefa0cdebe19cc69de5
Peter Kelly <peter@uxproductivity.com>
2012-01-11 18:13:38 +0000

    Small capitalisation fix (setStyleSheet instead of setStylesheet in preview.js), and removed some debugging output and obsolete, commented-out code.

--- src/Preview.js
+++ src/Preview.js
@@ -1,4 +1,4 @@
-function setStylesheet(selector,cssText)
+function setStyleSheet(selector,cssText)
 {
     var previewText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec in diam mauris. Integer in lorem sit amet dolor lacinia aliquet. Cras vehicula odio non enim euismod nec congue lorem varius. Sed eu libero arcu, eget tempus augue. Vivamus varius risus ac libero sagittis eu ultricies lectus consequat. Integer gravida accumsan fermentum. Morbi erat ligula, volutpat non accumsan sed, pellentesque quis purus. Vestibulum vestibulum tincidunt lectus non pellentesque. Quisque porttitor sollicitudin tellus, id porta velit interdum sit amet. Cras quis sem orci, vel convallis magna. Pellentesque congue, libero et iaculis volutpat, enim turpis sodales dui, lobortis pharetra lectus dolor at sem. Nullam aliquam, odio ac laoreet vulputate, ligula nunc euismod leo, vel bibendum magna leo ut orci. In tortor turpis, pellentesque nec cursus ut, consequat non ipsum. Praesent venenatis, leo in pulvinar pharetra, eros nisi convallis elit, vitae luctus magna velit ut lorem."
 
