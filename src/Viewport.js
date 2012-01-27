b629b901c421eef596a845a5ed66129bb73f540d
Peter Kelly <peter@uxproductivity.com>
2012-01-27 09:05:14 +0000

    Got rid of setVisibleArea(), which was used to report the size of the web view to javascript. It's no longer needed, because we always resize the UIWebView then the keyboard is shown or hidden, and thus window.innerWidth and window.innerHeight can be used to determine the region of the page that is visible on screen (as is done in selection.js)

--- src/Viewport.js
+++ src/Viewport.js
@@ -38,18 +38,3 @@ function setViewportWidth(width)
     viewportMetaElement.setAttribute("content","width = "+width);
     updateSelectionDisplay();
 }
-
-// These variables keep track of the portion of the window that is visible on screen (i.e. not
-// hidden by the keyboard). We need to keep track of these since scrollIntoViewIfNeeded() does
-// not take into account the presence of the keyboard on screen, and every time we move the
-// cursor we want to make sure it is visible *above* the keyboard.
-
-var visibleAreaWidth = null;
-var visibleAreaHeight = null;
-
-function setVisibleArea(width,height)
-{
-    visibleAreaWidth = width;
-    visibleAreaHeight = height;
-    updateSelectionDisplay();
-}
