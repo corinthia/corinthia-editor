72e779f21c65cbe1027c1f81183fbd5ca084fd6f
Peter Kelly <peter@uxproductivity.com>
2012-01-27 11:22:10 +0000

    Got the javascript tests working again after all the changes to the JS code since the move to iOS.

---
+++ tests/nulleditor.js
@@ -0,0 +1,73 @@
+var editor = {
+    debug: function(str)
+    {
+    },
+
+    getHTMLError: function(message)
+    {
+    },
+    
+    getHTMLResponse: function(html)
+    {
+    },
+    
+    jsInterfaceInitError: function(error)
+    {
+    },
+    
+    jsInterfaceInitFinished: function()
+    {
+    },
+    
+    reportStyle: function(style)
+    {
+    },
+    
+    reportFormatting: function(str)
+    {
+    },
+    
+    sectionAdded: function(name,id,level,prevId)
+    {
+    },
+    
+    sectionRemoved: function(id)
+    {
+    },
+    
+    updateSectionName: function(id,name)
+    {
+    },
+    
+    setOutline: function(outline)
+    {
+    },
+    
+    setStyles: function(styles)
+    {
+    },
+    
+    setCursor: function(x,y,height)
+    {
+    },
+    
+    setSelectionHandles: function(x1,y1,height1,x2,y2,height2)
+    {
+    },
+    
+    clearSelectionHandlesAndCursor: function()
+    {
+    },
+
+    setSelectionBounds: function(left,top,right,bottom)
+    {
+    },
+    
+    showCursorMenu: function(x,y,height)
+    {
+    },
+    
+    sayHello: function(name)
+    {
+    }
+}
