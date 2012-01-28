46694051e6562a48e12d05bc73bfcd84696a841b
Peter Kelly <peter@uxproductivity.com>
2012-01-28 15:32:13 +0000

    Moved the javascript code out of treeselection.html and testharness.html into separate .js files so that it can be searched with other js files using jsgrep.

---
+++ tests/testharness.js
@@ -0,0 +1,411 @@
+var editor = {
+    debug: function(str) { console.log(str); },
+    getHTMLError: function(message) {},
+    getHTMLResponse: function(html) {},
+    jsInterfaceInitError: function(error) {alert("js init error: "+error);},
+    jsInterfaceInitFinished: function() {},
+    reportStyle: function(style) {},
+    reportFormatting: function(str) {},
+    sectionAdded: function(name,id,level,prevId) {},
+    sectionRemoved: function(id) {},
+    updateSectionName: function(id,name) {},
+    setOutline: function(jsonOutline) {},
+    setStyles2: function(jsonStyles) {},
+    setStyles: function(jsonStyles) {}
+}
+
+var left;
+var right;
+var leftLoadedContinuation = null;
+var results = new Object();
+
+function Result(actual,expected)
+{
+    this.actual = actual;
+    this.expected = expected;
+}
+
+function readFile(filename)
+{
+    var req = new XMLHttpRequest();
+    req.open("GET",filename,false);
+    req.send();
+    return req.responseText;
+}
+
+function loadTestIndex()
+{
+    window.eval(readFile("index.js"));
+}
+
+function showTest(dir,name)
+{
+    leftLoadedContinuation = function() {
+        setLeftTitle("Working area");
+        setRightTitle("Result");
+        left.contentWindow.performTest();
+        left.contentWindow.clearSelection();
+        setPanelText(right,getHTML(left.contentDocument.documentElement));
+    }
+    left.src = dir+"/"+name+"-input.html";
+}
+
+function showResult(dirname,filename)
+{
+    var fullname = dirname+"-"+filename;
+    setLeftTitle("Actual result for "+dirname+"/"+filename);
+    setRightTitle("Expected result for "+dirname+"/"+filename);
+    setPanelText(left,results[fullname].actual);
+    setPanelText(right,results[fullname].expected);
+}
+
+function setLeftTitle(title)
+{
+    document.getElementById("leftTitle").firstChild.nodeValue = title;
+}
+
+function setRightTitle(title)
+{
+    document.getElementById("rightTitle").firstChild.nodeValue = title;
+}
+
+function clearPanel(panel)
+{
+    panel.contentDocument.open();
+    panel.contentDocument.close();
+}
+
+function setPanelText(panel,text)
+{
+    clearPanel(panel);
+    var pre = panel.contentDocument.createElement("PRE");
+    panel.contentDocument.body.appendChild(pre);
+    pre.appendChild(right.contentDocument.createTextNode(text));
+}
+
+function getHTML(root)
+{
+    var copy = root.cloneNode(true);
+    for (var body = copy.firstChild; body != null; body = body.nextSibling) {
+        if (body.nodeName == "BODY") {
+            body.removeAttribute("style");
+            body.removeAttribute("contentEditable");
+        }
+    }
+
+    var builder = new left.contentWindow.StringBuilder();
+    prettyPrint(builder,copy,"");
+    return builder.str;
+}
+
+function trim(str)
+{
+    var start = 0;
+    var end = str.length;
+
+    while ((start < str.length) &&
+           ((str.charAt(start) == " ") ||
+            (str.charAt(start) == "\t") ||
+            (str.charAt(start) == "\r") ||
+            (str.charAt(start) == "\n")))
+        start++;
+
+    while ((end > start) &&
+           ((str.charAt(end-1) == " ") ||
+            (str.charAt(end-1) == "\t") ||
+            (str.charAt(end-1) == "\r") ||
+            (str.charAt(end-1) == "\n")))
+        end--;
+
+    return str.slice(start,end);
+}
+
+function singleDescendents(node)
+{
+    var count = 0;
+    for (var child = node.firstChild; child != null; child = child.nextSibling) {
+        if ((child.nodeType == Node.TEXT_NODE) && (trim(child.nodeValue).length == 0))
+            continue;
+        count++;
+        if (count > 1)
+            return false;
+        if (!singleDescendents(child))
+            return false;
+    }
+    return true;
+}
+
+function attributeString(node)
+{
+    var names = new Array();
+    for (var i = 0; i < node.attributes.length; i++) {
+        names.push(node.attributes[i].nodeName);
+    }
+    names.sort();
+    var str = "";
+    for (var i = 0; i < names.length; i++) {
+        str += " "+names[i]+"=\""+node.getAttribute(names[i])+"\"";
+    }
+    return str;
+}
+
+function prettyPrintOneLine(builder,node)
+{
+    if ((node.nodeType == Node.ELEMENT_NODE) && (node.nodeName != "SCRIPT")) {
+        var name = node.nodeName.toLowerCase();
+        if (node.firstChild == null) {
+            builder.str += indent + "<" + name + attributeString(node) + "/>\n";
+        }
+        else {
+            builder.str += "<" + name + attributeString(node) + ">";
+            for (var child = node.firstChild; child != null; child = child.nextSibling)
+                prettyPrintOneLine(builder,child);
+            builder.str += "</" + name + ">";
+        }
+    }
+    else if (node.nodeType == Node.TEXT_NODE) {
+        var value = trim(node.nodeValue);
+        if (value.length > 0)
+            builder.str += value;
+    }
+    else if (node.nodeType == Node.COMMENT_NODE) {
+        builder.str += "<!--" + node.nodeValue + "-->\n";
+    }
+}
+
+function prettyPrint(builder,node,indent)
+{
+    if ((node.nodeType == Node.ELEMENT_NODE) && (node.nodeName != "SCRIPT")) {
+        var name = node.nodeName.toLowerCase();
+        if (node.firstChild == null) {
+            builder.str += indent + "<" + name + attributeString(node) + "/>\n";
+        }
+        else {
+            if (singleDescendents(node)) {
+                builder.str += indent;
+                prettyPrintOneLine(builder,node);
+                builder.str += "\n";
+            }
+            else {
+                builder.str += indent + "<" + name + attributeString(node) + ">\n";
+                for (var child = node.firstChild; child != null; child = child.nextSibling)
+                    prettyPrint(builder,child,indent+"  ");
+                builder.str += indent + "</" + name + ">\n";
+            }
+
+        }
+    }
+    else if (node.nodeType == Node.TEXT_NODE) {
+        var value = trim(node.nodeValue);
+        if (value.length > 0)
+            builder.str += indent + value + "\n";
+    }
+    else if (node.nodeType == Node.COMMENT_NODE) {
+        builder.str += indent + "<!--" + node.nodeValue + "-->\n";
+    }
+}
+
+function readJSCode(filename)
+{
+    var req = new XMLHttpRequest();
+    req.open("GET",filename,false);
+    req.send();
+    return req.responseText;
+}
+
+function leftLoaded()
+{
+    if (leftLoadedContinuation == null)
+        return;
+    var continuation = leftLoadedContinuation;
+    leftLoadedContinuation = null;
+
+    left.contentWindow.editor = editor;
+    left.contentWindow.debug = editor.debug;
+
+    // Sync with Editor.m
+    var javascriptFiles = ["nulleditor.js", // must be first
+                           "../src/cursor.js",
+                           "../src/ElementProxy.js",
+                           "../src/ElementProxyMap.js",
+                           "../src/Formatting.js",
+                           "../src/Lists.js",
+                           "../src/mutation.js",
+                           "../src/Outline.js",
+                           "../src/Position.js",
+                           "../src/Range.js",
+                           "../src/StringBuilder.js",
+                           "../src/Structure.js",
+                           "../src/Styles.js",
+                           "../src/traversal.js",
+                           "../src/types.js",
+                           "../src/util.js",
+                           "../src/Viewport.js",
+                           "../src/Selection.js",
+                           "../src/init.js"]; // must be last
+    for (var i = 0; i < javascriptFiles.length; i++)
+        left.contentWindow.eval(readJSCode(javascriptFiles[i]));
+
+    var selStart = left.contentDocument.getElementById("selStart");
+    var selEnd = left.contentDocument.getElementById("selEnd");
+    var cursor = left.contentDocument.getElementById("cursor");
+
+    if ((selStart != null) && (selEnd == null))
+        throw new Error("selStart specified, but not selEnd");
+    if ((selStart == null) && (selEnd != null))
+        throw new Error("selEnd specified, but not selStart");
+
+    if ((selStart != null) && (selEnd != null)) {
+        var start = getPosition(selStart);
+        var end = getPosition(selEnd);
+        if (start == null)
+            throw new Error("Could not find text node for selStart");
+        if (end == null)
+            throw new Error("Could not find text node for selEnd");
+
+        selStart.parentNode.removeChild(selStart);
+        selEnd.parentNode.removeChild(selEnd);
+
+        var range = new left.contentWindow.Range(start.node,start.offset,end.node,end.offset);
+        left.contentWindow.setSelectionRange(range);
+    }
+    continuation();
+
+    return;
+
+    function getPosition(node)
+    {
+        if ((node.nextSibling != null) &&
+            (node.nextSibling.nodeType == Node.TEXT_NODE)) {
+            return new left.contentWindow.Position(node.nextSibling,0);
+        }
+        else if ((node.previousSibling != null) &&
+            (node.previousSibling.nodeType == Node.TEXT_NODE)) {
+            return new left.contentWindow.Position(node.previousSibling,
+                                                   node.previousSibling.nodeValue.length);
+        }
+        else {
+            return null;
+        }
+    }
+}
+
+function runAllTests()
+{
+    var dirno = 0;
+    var fileno = 0;
+    var haveTest = false;
+    var dirname;
+    var filename;
+
+    setLeftTitle("Working area");
+    setRightTitle("");
+
+    clearPanel(right);
+    results = new Object();
+
+    runNextTest();
+    return;
+
+    function runNextTest()
+    {
+        if (haveTest) {
+            var expected = readFile(dirname+"/"+filename+"-expected.html");
+
+            var actual;
+            try {
+                left.contentWindow.performTest();
+                left.contentWindow.clearSelection();
+                actual = getHTML(left.contentDocument.documentElement);
+            }
+            catch (e) {
+                actual = e.toString();
+            }
+
+            actual = trim(actual);
+            expected = trim(expected);
+
+            var fullname = dirname+"-"+filename;
+            var resultElement = document.getElementById("result-"+fullname);
+            while (resultElement.firstChild != null)
+                resultElement.removeChild(resultElement.firstChild);
+            var a = document.createElement("a");
+            a.href = "javascript:showResult('"+dirname+"','"+filename+"')";
+            resultElement.appendChild(a);
+            results[fullname] = new Result(actual,expected);
+            if (actual == expected) {
+                resultElement.setAttribute("class","pass");
+                a.appendChild(document.createTextNode("PASS"));
+            }
+            else {
+                resultElement.setAttribute("class","fail");
+                a.appendChild(document.createTextNode("FAIL"));
+            }
+        }
+        if (dirno < tests.length) {
+            var dir = tests[dirno];
+            dirname = dir.dir;
+            filename = dir.files[fileno];
+            incrementPosition();
+            leftLoadedContinuation = runNextTest;
+            haveTest = true;
+            left.src = dirname+"/"+filename+"-input.html";
+        }
+    }
+
+    function incrementPosition()
+    {
+        fileno++;
+        if (fileno == tests[dirno].files.length) {
+            dirno++;
+            fileno = 0;
+        }
+    }
+}
+
+function loaded()
+{
+    var top = document.getElementById("topInner");
+    left = document.getElementById("leftInner");
+    right = document.getElementById("rightInner");
+    loadTestIndex();
+
+    var table = document.createElement("table");
+    top.appendChild(table);
+
+    for (var dirno = 0; dirno < tests.length; dirno++) {
+        var dir = tests[dirno];
+
+        var tr = document.createElement("tr");
+        table.appendChild(tr);
+        tr.setAttribute("class","dirrow");
+        table.setAttribute("width","100%");
+
+        var td = document.createElement("td");
+        tr.appendChild(td);
+        td.setAttribute("colspan","2");
+        td.appendChild(document.createTextNode(dir.dir));
+
+        for (var fileno = 0; fileno < dir.files.length; fileno++) {
+            var filename = dir.files[fileno];
+
+            tr = document.createElement("tr");
+            table.appendChild(tr);
+            tr.setAttribute("class","testrow");
+
+            td = document.createElement("td");
+            tr.appendChild(td);
+            td.setAttribute("width","50%");
+
+            var a = document.createElement("a");
+            td.appendChild(a);
+            a.href = "javascript:showTest('"+dir.dir+"','"+filename+"')";
+            a.appendChild(document.createTextNode(filename));
+
+            td = document.createElement("td");
+            tr.appendChild(td);
+            td.setAttribute("width","50%");
+            td.id = "result-"+dir.dir+"-"+filename;
+        }
+    }
+}
---
+++ tests/treeselection.js
@@ -0,0 +1,210 @@
+function debug(str)
+{
+    console.log(str);
+}
+
+var LABEL_HEIGHT = 10;
+
+var rootNode = null;
+var nextNodeId = 0;
+
+function createDiv()
+{
+    var node = document.createElement("DIV");
+    node.setAttribute("id",nextNodeId++);
+    node.setAttribute("class","node");
+    return node;
+}
+
+function addNodeAtDepth(depth)
+{
+    var parent = rootNode;
+    while ((depth > 0) && (parent.lastChild != null)) {
+        parent = parent.lastChild;
+        depth--;
+    }
+    while (depth > 0) {
+        var temp = createDiv();
+        parent.appendChild(temp);
+        depth--;
+    }
+    var temp = createDiv();
+    parent.appendChild(temp);
+}
+
+function layout(node,showLabel)
+{
+    for (var child = node.firstChild; child != null; child = child.nextSibling)
+        layout(child,showLabel);
+
+    if (node.firstChild == null)
+        node.style.height = LABEL_HEIGHT+"px";
+}
+
+var addedLabels = false;
+
+function addLabels(node)
+{
+    for (var child = node.firstChild; child != null; child = child.nextSibling)
+       addLabels(child);
+
+    if (node.nodeType == Node.ELEMENT_NODE) {
+        var label = document.createElement("DIV");
+        var rect = node.getBoundingClientRect();
+        label.setAttribute("class","label");
+        label.style.position = "absolute";
+        label.style.left = rect.left+"px";
+        label.style.top = rect.top+"px";
+        label.appendChild(document.createTextNode(node.getAttribute("id")));
+        document.body.appendChild(label);
+    }
+}
+
+var dragging = false;
+var selectionRange = null;
+
+function indexOfNodeIn(node,parent)
+{
+    var index = 0;
+    for (var child = parent.firstChild; child != null; child = child.nextSibling) {
+        if (child == node)
+            return index;
+        index++;
+    }
+    return null;
+}
+
+function adjustPosition(position,end)
+{
+    var parent = position.node.parentNode;
+    if (parent != null) {
+        var index = indexOfNodeIn(position.node,parent);
+        if (index == null)
+            throw new Error("Could not get index");
+
+        if (!end && (index > 0)) {
+            position.node = parent;
+            position.offset = index;
+        }
+        else if (end && (index < parent.childNodes.length-1)) {
+            position.node = parent;
+            position.offset = index+1;
+        }
+    }
+}
+
+function updateSelectionDisplay()
+{
+    clearHighlightsRecursive(rootNode);
+
+    if (selectionRange != null) {
+
+        var tempRange = selectionRange.copy();
+//        adjustPosition(tempRange.start,false);
+//        adjustPosition(tempRange.end,true);
+//        tempRange.convertToOffsetFree();
+        if (selectionRange.start.node != tempRange.start.node) {
+            debug("start.node mismatch (expected "+
+                  selectionRange.start.node.getAttribute("id")+", got "+
+                  tempRange.start.node.getAttribute("id")+")");
+        }
+        if (selectionRange.start.offset != tempRange.start.offset) {
+            debug("start.offset mismatch");
+        }
+        if (selectionRange.end.node != tempRange.end.node) {
+            debug("end.node mismatch (expected "+
+                  selectionRange.end.node.getAttribute("id")+", got "+
+                  tempRange.end.node.getAttribute("id")+")");
+        }
+        if (selectionRange.end.offset != tempRange.end.offset) {
+            debug("end.offset mismatch");
+        }
+
+        var useRange = selectionRange.copy();
+
+        useRange.start.node.style.border = "1px solid lime";
+        useRange.end.node.style.border = "1px solid red";
+
+        var nodes = useRange.getSelectedNodes();
+        for (var i = 0; i < nodes.length; i++) {
+            if (nodes[i].nodeType == Node.ELEMENT_NODE)
+                nodes[i].style.backgroundColor = "#c0c0c0";
+        }
+    }
+
+    function clearHighlightsRecursive(node)
+    {
+        if (node.nodeType == Node.ELEMENT_NODE) {
+            node.style.removeProperty("background-color");
+            node.style.removeProperty("border");
+            for (var child = node.firstChild; child != null; child = child.nextSibling)
+                clearHighlightsRecursive(child);
+        }
+    }
+}
+
+function mousedown(event)
+{
+    if (!addedLabels) {
+        addLabels(rootNode);
+        addedLabels = true;
+    }
+    if ((event.target.nodeName == "DIV") && (event.target.firstChild == null)) {
+        selectionRange = new Range(event.target,0,event.target,0);
+    }
+    else {
+        selectionRange = null;
+    }
+    updateSelectionDisplay();
+    dragging = true;
+}
+
+function mousemove(event)
+{
+    if (dragging) {
+        var node = event.target;
+        if ((node.nodeName == "DIV") && (node.firstChild == null)) {
+            if (selectionRange == null) {
+                selectionRange = new Range(node,0,node,0);
+            }
+            else {
+                selectionRange.end = new Position(node,node.childNodes.length);
+            }
+            updateSelectionDisplay();
+        }
+    }
+}
+
+function mouseup(event)
+{
+    dragging = false;
+}
+
+function loaded()
+{
+    rootNode = createDiv();
+
+    for (var i = 0; i < 4; i++) {
+        addNodeAtDepth(0);
+        addNodeAtDepth(0);
+        addNodeAtDepth(1);
+        addNodeAtDepth(1);
+        addNodeAtDepth(2);
+        addNodeAtDepth(2);
+        addNodeAtDepth(3);
+        addNodeAtDepth(3);
+        addNodeAtDepth(3);
+        addNodeAtDepth(2);
+        addNodeAtDepth(2);
+        addNodeAtDepth(1);
+        addNodeAtDepth(1);
+        addNodeAtDepth(0);
+    }
+
+    layout(rootNode);
+    document.body.appendChild(rootNode);
+
+    document.onmousedown = mousedown;
+    document.onmousemove = mousemove;
+    document.onmouseup = mouseup;
+}
