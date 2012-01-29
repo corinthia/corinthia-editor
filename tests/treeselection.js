03667e3dd1bfee2937671a1c1c1cba9f8057a68d
Peter Kelly <peter@uxproductivity.com>
2012-01-29 09:56:06 +0000

    Moved layout code into HorizontalLayout.js

--- tests/treeselection.js
+++ tests/treeselection.js
@@ -36,127 +36,7 @@ function addNodeAtDepth(depth)
     parent.appendChild(temp);
 }
 
-var layoutX = 0;
-var layoutY = 0;
 
-function layout(node)
-{
-    node.style.position = "absolute";
-    node.style.left = layoutX+"px";
-    node.style.top = layoutY+"px";
-    node.style.width = NODE_WIDTH+"px";
-    node.style.height = NODE_HEIGHT+"px";
-
-    if (node.firstChild == null) {
-        layoutX += NODE_WIDTH + SPACING;
-    }
-
-    var left = layoutX;
-    layoutY += NODE_HEIGHT + LEVEL_SPACING;
-    for (var child = node.firstChild; child != null; child = child.nextSibling) {
-        layout(child);
-    }
-    layoutY -= NODE_HEIGHT + LEVEL_SPACING;
-    var right = layoutX;
-
-    var width = right - left;
-
-    if (node.firstChild != null) {
-        node.style.left = (left + width/2 - NODE_WIDTH/2)+"px";
-    }
-}
-
-function adjustRelativePositions(node)
-{
-    for (var child = node.firstChild; child != null; child = child.nextSibling) {
-        adjustRelativePositions(child);
-
-        var relX = parseInt(child.style.left) - parseInt(node.style.left);
-        var relY = parseInt(child.style.top) - parseInt(node.style.top);
-        child.style.left = relX+"px";
-        child.style.top = relY+"px";
-    }
-}
-
-function addLines(node,offsetX,offsetY)
-{
-    if (node.firstChild != null) {
-        var nodeX = offsetX + parseInt(node.style.left) + 1;
-        var nodeY = offsetY + parseInt(node.style.top) + 1;
-
-        var childrenWidth = 0;
-        for (var child = node.firstChild; child != null; child = child.nextSibling) {
-            childrenWidth += addLines(child,nodeX,nodeY);
-        }
-
-
-        var firstChildX = nodeX + parseInt(node.firstChild.style.left) +
-                                  parseInt(node.firstChild.style.width)/2;
-        var lastChildX = nodeX + parseInt(node.lastChild.style.left) +
-                                 parseInt(node.lastChild.style.width)/2;
-        var lineWidth = lastChildX - firstChildX;
-
-
-        debug("addLines "+node.id+" offset "+offsetX+","+offsetY);
-        var nodeWidth = parseInt(node.style.width);
-
-        var horizLineDiv = document.createElement("DIV");
-        horizLineDiv.style.position = "absolute";
-        horizLineDiv.style.borderTop = "1px solid black";
-        horizLineDiv.style.width = lineWidth+"px";
-        horizLineDiv.style.height = "0px";
-        horizLineDiv.style.left = firstChildX+"px";
-        horizLineDiv.style.top = (nodeY + NODE_HEIGHT + LEVEL_SPACING/2)+"px";
-        document.body.appendChild(horizLineDiv);
-
-        for (var child = node.firstChild; child != null; child = child.nextSibling) {
-            var childX = parseInt(child.style.left);
-            var childWidth = parseInt(child.style.width);
-
-            var verticalLineDiv = document.createElement("DIV");
-            verticalLineDiv.style.position = "absolute";
-            verticalLineDiv.style.borderLeft = "1px solid black";
-            verticalLineDiv.style.width = "0px";
-            verticalLineDiv.style.height = (LEVEL_SPACING/2)+"px";
-            verticalLineDiv.style.top = (nodeY + NODE_HEIGHT + LEVEL_SPACING/2)+"px";
-            verticalLineDiv.style.left = (nodeX + childX + childWidth/2)+"px";
-            document.body.appendChild(verticalLineDiv);
-        }
-
-        var topLine = document.createElement("DIV");
-        topLine.style.position = "absolute";
-        topLine.style.borderLeft = "1px solid black";
-        topLine.style.width = "0px";
-        topLine.style.height = (LEVEL_SPACING/2)+"px";
-        topLine.style.top = (nodeY + NODE_HEIGHT)+"px";
-        topLine.style.left = (nodeX + parseInt(node.style.width)/2)+"px";
-        document.body.appendChild(topLine);
-
-        return childrenWidth;
-    }
-    else {
-        return parseInt(node.style.width);
-    }
-}
-
-function addLabels(node)
-{
-    for (var child = node.firstChild; child != null; child = child.nextSibling)
-       addLabels(child);
-
-    if (node.nodeType == Node.ELEMENT_NODE) {
-        var label = document.createElement("DIV");
-        var rect = node.getBoundingClientRect();
-        label.setAttribute("class","label");
-        label.style.position = "absolute";
-        label.style.left = rect.left+"px";
-        label.style.top = rect.top+"px";
-        label.style.width = node.style.width;
-        label.style.textAlign = "center";
-        label.appendChild(document.createTextNode(node.getAttribute("id")));
-        document.body.appendChild(label);
-    }
-}
 
 var dragging = false;
 var selectionRange = null;
@@ -312,12 +192,9 @@ function loaded()
     }
 
     document.body.appendChild(rootNode);
-    layout(rootNode);
-    adjustRelativePositions(rootNode);
-    rootNode.style.left = (parseInt(rootNode.style.left) + 20)+"px";
-    rootNode.style.top = (parseInt(rootNode.style.top) + 40)+"px";
-    addLines(rootNode,0,0);
-    addLabels(rootNode);
+
+    var layout = new HorizontalLayout();
+    layout.layoutNodes(rootNode);
 
     document.onmousedown = mousedown;
     document.onmousemove = mousemove;
