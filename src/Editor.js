2cb8c1a204110303c7db3cacf072290ce84ce9b6
Peter Kelly <peter@uxproductivity.com>
2012-04-08 09:51:14 +0000

    Wrapped the Editor definitions in an anonymous function scope, so that the backMessages array and addBackMessage() functions are private.

--- src/Editor.js
+++ src/Editor.js
@@ -1,86 +1,67 @@
 //  Copyright (c) 2011-2012 UX Productivity Pty Ltd. All rights reserved.
 
-var backMessages = new Array();
+(function(){
 
-function getBackMessages()
-{
-    var result = JSON.stringify(backMessages);
-    backMessages = new Array();
-    return result;
-}
+    var backMessages = new Array();
 
-function addBackMessage()
-{
-    backMessages.push(arrayCopy(arguments));
-    return null;
-}
+    function addBackMessage()
+    {
+        backMessages.push(arrayCopy(arguments));
+        return null;
+    }
 
-var Editor = {
-    debug: function(str)
-    {
-        addBackMessage("debug",str);
-    },
+    window.Editor = {
 
-    setOutline: function(outline)
-    {
-        addBackMessage("setOutline",outline);
-    },
-    
-    setStyles: function(styles)
-    {
-        addBackMessage("setStyles",styles);
-    },
-    
-    setCursor: function(x,y,width,height)
-    {
-        addBackMessage("setCursor",x,y,width,height);
-    },
-    
-    setSelectionHandles: function(x1,y1,height1,x2,y2,height2)
-    {
-        addBackMessage("setSelectionHandles",x1,y1,height1,x2,y2,height2);
-    },
-    
-    clearSelectionHandlesAndCursor: function()
-    {
-        addBackMessage("clearSelectionHandlesAndCursor");
-    },
+        getBackMessages: function()
+        {
+            var result = JSON.stringify(backMessages);
+            backMessages = new Array();
+            return result;
+        },
 
-    setSelectionBounds: function(left,top,right,bottom)
+        debug: function(str)
+        {
+            addBackMessage("debug",str);
+        },
+
+        setOutline: function(outline)
+        {
+            addBackMessage("setOutline",outline);
+        },
+        
+        setStyles: function(styles)
+        {
+            addBackMessage("setStyles",styles);
+        },
+        
+        setCursor: function(x,y,width,height)
+        {
+            addBackMessage("setCursor",x,y,width,height);
+        },
+        
+        setSelectionHandles: function(x1,y1,height1,x2,y2,height2)
+        {
+            addBackMessage("setSelectionHandles",x1,y1,height1,x2,y2,height2);
+        },
+        
+        clearSelectionHandlesAndCursor: function()
+        {
+            addBackMessage("clearSelectionHandlesAndCursor");
+        },
+
+        setSelectionBounds: function(left,top,right,bottom)
+        {
+            addBackMessage("setSelectionBounds",left,top,right,bottom);
+        },
+        
+        reportJSError: function(error)
+        {
+            addBackMessage("reportJSError",error.toString());
+        },    
+    }
+
+    window.debug = function(str)
     {
-        addBackMessage("setSelectionBounds",left,top,right,bottom);
-    },
-    
-    reportJSError: function(error)
-    {
-        addBackMessage("reportJSError",error.toString());
-    },    
-}
-
-function debug(str)
-{
-    Editor.debug(str);
-}
-
-function warning(str)
-{
-    debug("WARNING: "+str);
-}
-
-//function showProperties(object,objname)
-//{
-//    var functions = new Array();
-//    var properties = new Array();
-//    for (var name in object) {
-//        if (typeof(object[name]) == "function")
-//            functions.push(name);
-//        else
-//            properties.push(name);
-//    }
-//    functions.sort();
-//    properties.sort();
-//    for (var i = 0; i < functions.length; i++)
-//        debug(objname+"."+functions[i]+"()");
-//    for (var i = 0; i < properties.length; i++)
-//        debug(objname+"."+properties[i]);
-//}
+        Editor.debug(str);
+    }
+})();
