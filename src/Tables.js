baa30cf41476c804f279526bd968e58199cc1cb7
Peter Kelly <peter@uxproductivity.com>
2012-04-13 04:14:50 +0000

    Added Tables_analyseStructure, with code from docx/htmltable.js

--- src/Tables.js
+++ src/Tables.js
@@ -10,9 +10,90 @@ var Tables_deleteColumns;
 var Tables_clearCells;
 var Tables_mergeCells;
 var Tables_splitCell;
+var Tables_analyseStructure;
 
 (function() {
 
+    function Cell(element,row,col)
+    {
+        this.element = element;
+        this.row = row;
+        this.col = col;
+
+        if (element.hasAttribute("colspan"))
+            this.colspan = parseInt(element.getAttribute("colspan"));
+        else
+            this.colspan = 1;
+        if (element.hasAttribute("rowspan"))
+            this.rowspan = parseInt(element.getAttribute("rowspan"));
+        else
+            this.rowspan = 1;
+
+        if (this.colspan < 1)
+            this.colspan = 1;
+        if (this.rowspan < 1)
+            this.rowspan = 1;
+    }
+
+    function Table(element)
+    {
+        this.row = 0;
+        this.col = 0;
+        this.cells = new Array();
+        this.numRows = 0;
+        this.numCols = 0;
+        this.translated = false;
+        this.cellsByElement = new NodeMap();
+        this.processTable(element);
+    }
+
+    Table.prototype.set = function(row,col,cell)
+    {
+        if (this.numRows < row+1)
+            this.numRows = row+1;
+        if (this.numCols < col+1)
+            this.numCols = col+1;
+        if (this.cells[row] == null)
+            this.cells[row] = new Array();
+        this.cells[row][col] = cell;
+    }
+
+    Table.prototype.get = function(row,col)
+    {
+        if (this.cells[row] == null)
+            return null;
+        return this.cells[row][col];
+    }
+
+    Table.prototype.processTable = function(node)
+    {
+        if ((DOM_upperName(node) == "TD") || (DOM_upperName(node) == "TH")) {
+            var cell = new Cell(node,this.row,this.col);
+            this.cellsByElement.put(node,cell);
+
+            while (this.get(this.row,this.col) != null)
+                this.col++;
+
+            for (var r = 0; r < cell.rowspan; r++) {
+                for (var c = 0; c < cell.colspan; c++) {
+                    this.set(this.row+r,this.col+c,cell);
+                }
+            }
+            this.col += cell.colspan;
+        }
+        else if (DOM_upperName(node) == "TR") {
+            for (var child = node.firstChild; child != null; child = child.nextSibling)
+                this.processTable(child);
+            this.row++;
+            this.col = 0;
+        }
+        else {
+            for (var child = node.firstChild; child != null; child = child.nextSibling)
+                this.processTable(child);
+        }
+    }
+
+    // public
     function insertTable(rows,cols,numbered,caption)
     {
         var table = DOM_createElement(document,"TABLE");
@@ -45,40 +126,55 @@ var Tables_splitCell;
         Clipboard_pasteNodes([table]);
     }
 
+    // public
     function insertRowsAbove(rows)
     {
     }
 
+    // public
     function insertRowsBelow(rows)
     {
     }
 
+    // public
     function insertColumnsLeft(cols)
     {
     }
 
+    // public
     function insertColumnsRight(cols)
     {
     }
 
+    // public
     function deleteRows()
     {
     }
 
+    // public
     function deleteColumns()
     {
     }
 
+    // public
     function clearCells()
     {
     }
 
+    // public
     function mergeCells()
     {
     }
 
+    // public
     function splitCell()
     {
+    }
+
+    // public
+    function analyseStructure(element)
+    {
+        return new Table(element);
     }
 
     Tables_insertTable = trace(insertTable);
@@ -91,5 +187,6 @@ var Tables_splitCell;
     Tables_clearCells = trace(clearCells);
     Tables_mergeCells = trace(mergeCells);
     Tables_splitCell = trace(splitCell);
+    Tables_analyseStructure = trace(analyseStructure);
 
 })();
