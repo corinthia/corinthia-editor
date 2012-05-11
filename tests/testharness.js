42a05337512d515552aed00e4e51c6b9996961ac
Peter Kelly <peter@uxproductivity.com>
2012-05-11 20:04:46 +0000

    Removed AutoCorrect.js for the time being, since I want to keep the autocorrect mechanism simple for the first release. We'll add it back in the future.

--- tests/testharness.js
+++ tests/testharness.js
@@ -23,7 +23,6 @@ function loadCode()
 {
     // Sync with Editor.m
     var javascriptFiles = ["../src/first.js", // must be first
-                           "../src/AutoCorrect.js",
                            "../src/Clipboard.js",
                            "../src/Cursor.js",
                            "../src/DOM.js",
