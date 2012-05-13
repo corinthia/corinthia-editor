87cc2b7f79ad40797201aeb665e2fe173faafe87
Peter Kelly <peter@uxproductivity.com>
2012-05-13 10:32:32 +0000

    Rewrote the undo manager to operate in a simpler manner. There's no need to support nested groups or object monitoring for what we want to do.

--- src/UndoManager.js
+++ src/UndoManager.js
@@ -1,245 +1,34 @@
 // Copyright (c) 2011-2012 UX Productivity Pty Ltd. All rights reserved.
 
-var UndoManager_monitors;
-var UndoManager_monitorObject;
-var UndoManager_unmonitorObject;
-var UndoManager_monitorWhileExecuting;
 var UndoManager_getLength;
 var UndoManager_getIndex;
 var UndoManager_setIndex;
 var UndoManager_print;
 var UndoManager_undo;
 var UndoManager_redo;
-var UndoManager_newGroup;
 var UndoManager_addAction;
-var UndoManager_group;
+var UndoManager_newGroup;
+var UndoManager_groupType;
 
 (function() {
 
-    function Monitor(id,object,property)
+    function UndoGroup(type)
     {
-        this.id = id;
-        this.object = object;
-        this.property = property;
-    }
-
-    Monitor.prototype.toString = function()
-    {
-        return "[Monitor "+this.property+"]";
-    }
-
-    var nextTrackingId = 0;
-    var monitors = UndoManager_monitors = new Object(); // public
-
-    function monitorProperty(object,name)
-    {
-        var _name = "__"+name;
-
-        // Check that we're not already being tracked
-        if (object[_name+"_oldDescriptor"] != null)
-            throw new Error("Property \""+name+"\" is already being tracked");
-
-        // Save original descriptor
-        object[_name+"_oldDescriptor"] = Object.getOwnPropertyDescriptor(object,name);
-
-        // Record property as tracked
-        var monitoredProperty = new Monitor(nextTrackingId++,object,name);
-        monitors[monitoredProperty.id] = monitoredProperty;
-        object[_name+"_monitoredProperty"] = monitoredProperty;
-
-        // Move value to hidden property
-        object[_name] = object[name];
-        delete object[name];
-
-        // Establish new property descriptor with getter and setters
-        var descriptor = new Object();
-
-        descriptor.get = function() {
-            //        debug("get: "+name);
-            return object[_name];
-        };
-
-        descriptor.set = function(newValue) {
-            var oldValue = object[_name];
-            UndoManager_addAction(function() {
-                object[name] = oldValue;
-            },"Set "+name+" to "+oldValue);
-            //        debug("set: "+name+" = "+newValue);
-            object[_name] = newValue;
-        };
-
-        descriptor.enumerable = true;
-        descriptor.configurable = true;
-        Object.defineProperty(object,name,descriptor);
-    }
-
-    function unmonitorProperty(object,name)
-    {
-        var _name = "__"+name;
-
-        // Restore original descriptor
-        var oldDescriptor = object[_name+"_oldDescriptor"];
-        if (oldDescriptor == null) {
-            // Property has been added since tracking started, and is thus not being tracked.
-            // Don't try to untrack it.
-            return;
-        }
-        delete object[_name+"_oldDescriptor"];
-        Object.defineProperty(object,name,oldDescriptor);
-
-        // Remove record of tracked property
-        var monitoredProperty = object[_name+"_monitoredProperty"];
-        delete object[_name+"_monitoredProperty"];
-        delete monitors[monitoredProperty.id];
-
-        // Restore original value
-        var value = object[_name];
-        delete object[_name];
-        object[name] = value;
-    }
-
-    // public
-    function monitorObject(object)
-    {
-        var names = Object.getOwnPropertyNames(object);
-
-        UndoManager_addAction(function() {
-            UndoManager_unmonitorObject(object);
-        },"Untrack object");
-
-        for (var i = 0; i < names.length; i++)
-            monitorProperty(object,names[i]);
-    }
-
-    // public
-    function unmonitorObject(object)
-    {
-        UndoManager_addAction(function() {
-            UndoManager_monitorObject(object);
-        },"Track object");
-
-        var names = Object.getOwnPropertyNames(object);
-        for (var i = 0; i < names.length; i++) {
-            if (names[i].indexOf("__") != 0)
-                unmonitorProperty(object,names[i]);
-        }
-    }
-
-    // public
-    function monitorWhileExecuting(object,fun)
-    {
-        UndoManager_monitorObject(object);
-        try {
-            return fun();
-        }
-        finally {
-            UndoManager_unmonitorObject(object);
-        }
-    }
-
-
-
-    function UndoGroup(direction,name)
-    {
-        this.name = name;
+        this.type = type;
         this.actions = new Array();
-        this.parentGroup = null;
-        this.direction = direction;
     }
 
-    UndoGroup.BACKWARDS = new Object();
-    UndoGroup.FORWARDS = new Object();
-
-    UndoGroup.prototype.addAction = function(action)
-    {
-        this.actions.push(action);
-    }
-
-    UndoGroup.prototype.perform = function(context)
-    {
-        enterGroup(this.name);
-        for (var i = this.actions.length-1; i >= 0; i--)
-            this.actions[i].perform(context);
-        exitGroup();
-    }
-
-    UndoGroup.prototype.print = function(indent,index)
-    {
-        if (this.direction == UndoGroup.FORWARDS) {
-            debug(indent+"Forwards group "+index+": "+this.name);
-            for (var i = 0; i < this.actions.length; i++)
-                this.actions[i].print(indent+"    ",i);
-        }
-        else if (this.direction == UndoGroup.BACKWARDS) {
-            debug(indent+"Backwards group "+index+": "+this.name);
-            for (var i = 0; i < this.actions.length; i++)
-                this.actions[i].print(indent+"    ",i);
-        }
-    }
-
-
-
-    function UndoAction(fun,name)
+    function UndoAction(fun,description)
     {
         this.fun = fun;
-        this.name = name;
+        this.description = description;
     }
 
-    UndoAction.prototype.perform = function(context)
-    {
-        //debug(context+": performing action \""+this.name+"\"");
-        this.fun();
-    }
-
-    UndoAction.prototype.print = function(indent,index)
-    {
-        debug(indent+"Action "+index+": "+this.name);
-    }
-
-
-
-    var inUndo = false;
-    var inRedo = false;
     var undoStack = new Array();
     var redoStack = new Array();
+    var inUndo = false;
+    var inRedo = false;
     var currentGroup = null;
-
-    function enterGroup(name)
-    {
-        var group;
-        if (inUndo)
-            group = new UndoGroup(UndoGroup.BACKWARDS,name);
-        else
-            group = new UndoGroup(UndoGroup.FORWARDS,name);
-        group.parentGroup = currentGroup;
-        if (currentGroup != null) {
-            currentGroup.addAction(group);
-        }
-        else if (inUndo) {
-            redoStack.push(group);
-        }
-        else if (inRedo) {
-            undoStack.push(group);
-        }
-        else {
-            redoStack.length = 0;
-            undoStack.push(group);
-        }
-        currentGroup = group;
-    }
-
-    function exitAllGroups()
-    {
-        while (currentGroup != null)
-            exitGroup();
-    }
-
-    function exitGroup()
-    {
-        if (currentGroup != null) {
-            currentGroup = currentGroup.parentGroup;
-        }
-    }
 
     // public
     function getLength()
@@ -256,104 +45,116 @@ var UndoManager_group;
     // public
     function setIndex(index)
     {
-        var length = UndoManager_getLength();
-        while ((undoStack.length < index) && (redoStack.length > 0))
-            UndoManager_redo();
-        while ((redoStack.length < length - index) && (undoStack.length > 0))
+        while (undoStack.length > index)
             UndoManager_undo();
+        while (undoStack.length < index)
+            UndoManager_redo();
     }
 
     // public
     function print()
     {
+        debug("");
+        debug("--------------------------------------------------------------------");
         debug("Undo stack:");
-        for (var i = 0; i < undoStack.length; i++)
-            undoStack[i].print("    ",i);
+        for (var groupIndex = 0; groupIndex < undoStack.length; groupIndex++) {
+            var group = undoStack[groupIndex];
+            debug("    "+group.type);
+            for (var actionIndex = 0; actionIndex < group.actions.length; actionIndex++) {
+                var action = group.actions[actionIndex];
+                debug("        "+action.description);
+            }
+        }
         debug("Redo stack:");
-        for (var i = 0; i < redoStack.length; i++)
-            redoStack[i].print("    ",i);
+        for (var groupIndex = 0; groupIndex < redoStack.length; groupIndex++) {
+            var group = redoStack[groupIndex];
+            debug("    "+group.type);
+            for (var actionIndex = 0; actionIndex < group.actions.length; actionIndex++) {
+                var action = group.actions[actionIndex];
+                debug("        "+action.description);
+            }
+        }
+        debug("Current group = "+currentGroup);
+        debug("--------------------------------------------------------------------");
+        debug("");
     }
 
     // public
     function undo()
     {
-        // FIXME: if the group is empty, undo() should skip it
+        currentGroup = null;
         if (undoStack.length > 0) {
-            exitAllGroups();
-            inUndo = true;
             var group = undoStack.pop();
-            try {
-                group.perform("undo");
-            }
-            finally {
-                exitAllGroups();
-                inUndo = false;
-            }
+            inUndo = true;
+            for (var i = group.actions.length-1; i >= 0; i--)
+                group.actions[i].fun();
+            inUndo = false;
         }
+        currentGroup = null;
     }
 
     // public
     function redo()
     {
+        currentGroup = null;
         if (redoStack.length > 0) {
-            exitAllGroups();
-            inRedo = true;
             var group = redoStack.pop();
-            try {
-                group.perform("redo");
-            }
-            finally {
-                exitAllGroups();
-                inRedo = false;
-            }
+            inRedo = true;
+            for (var i = group.actions.length-1; i >= 0; i--)
+                group.actions[i].fun();
+            inRedo = false;
         }
+        currentGroup = null;
     }
 
     // public
-    function newGroup(name)
+    function addAction(fun,description)
     {
-        if (currentGroup != null)
-            exitGroup();
-        enterGroup(name);
-    }
+        if (!inUndo && !inRedo && (redoStack.length > 0))
+            redoStack.length = 0;
 
-    // public
-    function addAction(fun,name)
-    {
-        //debug("Undo action: "+name);
+        var stack = inUndo ? redoStack : undoStack;
         if (currentGroup == null)
-            enterGroup();
-        currentGroup.addAction(new UndoAction(fun,name));
+            UndoManager_newGroup(null);
+
+        if (currentGroup.actions.length == 0) {
+            stack.push(currentGroup);
+        }
+
+        var action = new UndoAction(fun,description);
+        currentGroup.actions.push(action);
     }
 
     // public
-    function group(fun)
+    function newGroup(type)
     {
-        // We enter two levels of grouping here, so that if fun calls newGroup(), it will still
-        // remain within the scope of the outer group defined here.
-        enterGroup();
-        enterGroup();
-        try {
-            return fun.apply(null,Array.prototype.slice.call(arguments,1));
-        }
-        finally {
-            exitGroup();
-            exitGroup();
-        }
+        // We don't actually add the group to the undo stack until the first request to add an
+        // action to it. This way we don't end up with empty groups in the undo stack, which
+        // simplifies logic for moving back and forward through the undo history.
+
+        if ((type == null) || (type == ""))
+            type = "Anonymous";
+        currentGroup = new UndoGroup(type);
     }
 
-    UndoManager_monitorObject = trace(monitorObject);
-    UndoManager_unmonitorObject = trace(unmonitorObject);
-    UndoManager_monitorWhileExecuting = trace(monitorWhileExecuting);
+    // public
+    function groupType()
+    {
+        if (undoStack.length > 0)
+            return undoStack[undoStack.length-1].type;
+        else
+            return null;
+    }
+
     UndoManager_getLength = trace(getLength);
     UndoManager_getIndex = trace(getIndex);
     UndoManager_setIndex = trace(setIndex);
     UndoManager_print = trace(print);
     UndoManager_undo = trace(undo);
     UndoManager_redo = trace(redo);
-    UndoManager_newGroup = trace(newGroup);
     UndoManager_addAction = trace(addAction);
-    UndoManager_group = trace(group);
+    UndoManager_newGroup = trace(newGroup);
+    UndoManager_groupType = trace(groupType);
 
 })();
 
