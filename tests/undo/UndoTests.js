5355e1be237a1c3319089f354128bdedf378b5c8
Peter Kelly <peter@uxproductivity.com>
2012-05-23 18:56:13 +0000

    Changed the format of testUndo() to be more conscise, simply checking that the relevant DOM subtree matches after an undo or redo operation, rather than printing the whole thing out again each time.

--- tests/undo/UndoTests.js
+++ tests/undo/UndoTests.js
@@ -2,9 +2,9 @@ function testUndo(versions,node)
 {
     var numSteps = UndoManager_getLength();
 
-    var backwards1 = new Array();
+    var back1 = new Array();
     var forwards2 = new Array();
-    var backwards2 = new Array();
+    var back2 = new Array();
 
     var expected = new Array();
     for (var i = 0; i < versions.length; i++)
@@ -13,39 +13,50 @@ function testUndo(versions,node)
     for (var i = 0; i < numSteps; i++) {
         UndoManager_undo();
         PostponedActions_perform();
-        if (PrettyPrinter.getHTML(node) == expected[versions.length-2-i])
-            backwards1.push(DOM_createTextNode(document,"OK"));
+        var version = versions.length-2-i;
+        if (PrettyPrinter.getHTML(node) == expected[version])
+            back1.push(DOM_createTextNode(document,"First undo to version "+version+": OK"));
         else
-            backwards1.push(DOM_createTextNode(document,"INVALID"));
-        backwards1.push(DOM_cloneNode(node,true));
+            back1.push(DOM_createTextNode(document,"First undo to version "+version+": INVALID"));
     }
 
     for (var i = 0; i < numSteps; i++) {
         UndoManager_redo();
         PostponedActions_perform();
-        if (PrettyPrinter.getHTML(node) == expected[i+1])
-            forwards2.push(DOM_createTextNode(document,"OK"));
+        var version = i+1;
+        if (PrettyPrinter.getHTML(node) == expected[version])
+            forwards2.push(DOM_createTextNode(document,"Redo to version "+version+": OK"));
         else
-            forwards2.push(DOM_createTextNode(document,"INVALID"));
-        forwards2.push(DOM_cloneNode(node,true));
+            forwards2.push(DOM_createTextNode(document,"Redo to version "+version+": INVALID"));
     }
 
     for (var i = 0; i < numSteps; i++) {
         UndoManager_undo();
         PostponedActions_perform();
-        if (PrettyPrinter.getHTML(node) == expected[versions.length-2-i])
-            backwards2.push(DOM_createTextNode(document,"OK"));
+        var version = versions.length-2-i;
+        if (PrettyPrinter.getHTML(node) == expected[version])
+            back2.push(DOM_createTextNode(document,"Second undo to version "+version+": OK"));
         else
-            backwards2.push(DOM_createTextNode(document,"INVALID"));
-        backwards2.push(DOM_cloneNode(node,true));
+            back2.push(DOM_createTextNode(document,"Second undo to version "+version+": INVALID"));
     }
 
-    Array.prototype.push.apply(versions,backwards1);
+    var initialLength = versions.length;
+
+    Array.prototype.push.apply(versions,back1);
     Array.prototype.push.apply(versions,forwards2);
-    Array.prototype.push.apply(versions,backwards2);
+    Array.prototype.push.apply(versions,back2);
 
     Outline_removeListeners(); // prevent it from adding number spans etc.
     DOM_deleteAllChildren(document.body);
-    for (var i = 0; i < versions.length; i++)
+    for (var i = 0; i < versions.length; i++) {
+        if (i < initialLength) {
+            var str = "==================== Version "+i+" ====================";
+            DOM_appendChild(document.body,DOM_createTextNode(document,str));
+        }
+        else if (i == initialLength) {
+            var str = "===================================================";
+            DOM_appendChild(document.body,DOM_createTextNode(document,str));
+        }
         DOM_appendChild(document.body,versions[i]);
+    }
 }
