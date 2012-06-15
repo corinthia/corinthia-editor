a21ede0a118f99c1f95fe21221b0d1c3923971a3
Peter Kelly <peter@uxproductivity.com>
2012-06-15 03:27:02 +0000

    Part 2 of preparations for Selection_get() restrictions
    Don't directly call Selection_get() in the regionSpan or regionFromRange tests. Instead,
    call showSelectedTableRegion() or getSelectedTableRegion(), which are implemented in
    TableTests.js

--- tests/tables/TableTests.js
+++ tests/tables/TableTests.js
@@ -1,9 +1,19 @@
-function showTableRegion(region)
+function showSelectedTableRegion()
 {
-    for (var row = region.top; row <= region.bottom; row++) {
-        for (var col = region.left; col <= region.right; col++) {
-            var cell = region.structure.get(row,col);
-            DOM_setStyleProperties(cell.element,{"background-color": "silver"});
+    Selection_hideWhileExecuting(function() {
+        var region = Tables_regionFromRange(Selection_get());
+        for (var row = region.top; row <= region.bottom; row++) {
+            for (var col = region.left; col <= region.right; col++) {
+                var cell = region.structure.get(row,col);
+                DOM_setStyleProperties(cell.element,{"background-color": "silver"});
+            }
         }
-    }
+    });
+}
+
+function getSelectedTableRegion()
+{
+    return Selection_hideWhileExecuting(function() {
+        return Tables_regionFromRange(Selection_get());
+    });
 }
