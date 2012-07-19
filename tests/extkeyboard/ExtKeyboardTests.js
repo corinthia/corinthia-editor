3432ffc24354b5445536a441ba9034bf847136c5
Peter Kelly <peter@uxproductivity.com>
2012-07-19 18:29:47 +0000

    Moved the common code from the moveToStartOfWord() tests into ExtKeyboardTests.js

---
+++ tests/extkeyboard/ExtKeyboardTests.js
@@ -0,0 +1,45 @@
+function getNodeArrayText(nodes)
+{
+    var strings = new Array();
+    for (var i = 0; i < nodes.length; i++)
+        strings.push(getNodeText(nodes[i]));
+    return strings.join("");
+}
+
+function textBetweenPositions(from,to)
+{
+    var range = new Range(from.node,from.offset,to.node,to.offset);
+    var contents = range.cloneContents();
+    return getNodeArrayText(contents);
+}
+
+function testCursorMovement(movementFun)
+{
+    var start = new Position(document.body,0);
+    var end = new Position(document.body,document.body.childNodes.length);
+    var lines = new Array();
+
+
+    for (var pos = start; pos != null; pos = pos.next()) {
+
+        var oldBefore = textBetweenPositions(start,pos);
+        var oldAfter = textBetweenPositions(pos,end);
+        var oldTotal = oldBefore+"|"+oldAfter;
+
+
+        Selection_set(pos.node,pos.offset,pos.node,pos.offset);
+        movementFun();
+        var newPos = Selection_get().start;
+        
+        var newBefore = textBetweenPositions(start,newPos);
+        var newAfter = textBetweenPositions(newPos,end);
+        var newTotal = newBefore+"|"+newAfter;
+
+
+        lines.push(JSON.stringify(oldTotal.trim())+
+                   " -> "+
+                   JSON.stringify(newTotal.trim()));
+    }
+
+    return lines.join("\n");
+}
