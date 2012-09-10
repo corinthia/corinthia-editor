344f9c30f63e0ba4c20384c8afde4d167b31d8c7
Peter Kelly <peter@uxproductivity.com>
2012-09-10 19:14:59 +0000

    Introduction of type ids part 3: Added a bunch of arrays to types.js to use to check if a particular type id is of a certain kind (container, paragraph, heading etc.). Also modified all of the is* functions to use the numeric type id instead of the name.
    Reduces time for Outline_init() call from 4018ms to 2026ms on my thesis
    (tested on iPad 3, iOS 6 beta 4)

--- src/types.js
+++ src/types.js
@@ -21,6 +21,26 @@ var CONTAINER_ELEMENTS = {
     "NAV": true,
 };
 
+var CONTAINER_ELEMENT_TYPES = new Array(HTML_COUNT);
+CONTAINER_ELEMENT_TYPES[HTML_DOCUMENT] = true;
+CONTAINER_ELEMENT_TYPES[HTML_HTML] = true;
+CONTAINER_ELEMENT_TYPES[HTML_BODY] = true;
+CONTAINER_ELEMENT_TYPES[HTML_UL] = true;
+CONTAINER_ELEMENT_TYPES[HTML_OL] = true,
+CONTAINER_ELEMENT_TYPES[HTML_LI] = true;
+CONTAINER_ELEMENT_TYPES[HTML_TABLE] = true;
+CONTAINER_ELEMENT_TYPES[HTML_CAPTION] = true;
+CONTAINER_ELEMENT_TYPES[HTML_THEAD] = true;
+CONTAINER_ELEMENT_TYPES[HTML_TFOOT] = true;
+CONTAINER_ELEMENT_TYPES[HTML_TBODY] = true;
+CONTAINER_ELEMENT_TYPES[HTML_TR] = true;
+CONTAINER_ELEMENT_TYPES[HTML_TH] = true;
+CONTAINER_ELEMENT_TYPES[HTML_TD] = true;
+CONTAINER_ELEMENT_TYPES[HTML_COL] = true;
+CONTAINER_ELEMENT_TYPES[HTML_FIGURE] = true;
+CONTAINER_ELEMENT_TYPES[HTML_FIGCAPTION] = true;
+CONTAINER_ELEMENT_TYPES[HTML_NAV] = true;
+
 var PARAGRAPH_ELEMENTS = {
     "P": true,
     "H1": true,
@@ -34,6 +54,19 @@ var PARAGRAPH_ELEMENTS = {
     "BLOCKQUOTE": true
 };
 
+var PARAGRAPH_ELEMENT_TYPES = new Array(HTML_COUNT);
+PARAGRAPH_ELEMENT_TYPES[HTML_P] = true;
+PARAGRAPH_ELEMENT_TYPES[HTML_H1] = true;
+PARAGRAPH_ELEMENT_TYPES[HTML_H2] = true;
+PARAGRAPH_ELEMENT_TYPES[HTML_H3] = true;
+PARAGRAPH_ELEMENT_TYPES[HTML_H4] = true;
+PARAGRAPH_ELEMENT_TYPES[HTML_H5] = true;
+PARAGRAPH_ELEMENT_TYPES[HTML_H6] = true;
+PARAGRAPH_ELEMENT_TYPES[HTML_DIV] = true;
+PARAGRAPH_ELEMENT_TYPES[HTML_PRE] = true;
+PARAGRAPH_ELEMENT_TYPES[HTML_BLOCKQUOTE] = true;
+
+
 var HEADING_ELEMENTS = {
     "H1": true,
     "H2": true,
@@ -43,6 +76,14 @@ var HEADING_ELEMENTS = {
     "H6": true
 }
 
+var HEADING_ELEMENT_TYPES = new Array(HTML_COUNT);
+HEADING_ELEMENT_TYPES[HTML_H1] = true;
+HEADING_ELEMENT_TYPES[HTML_H2] = true;
+HEADING_ELEMENT_TYPES[HTML_H3] = true;
+HEADING_ELEMENT_TYPES[HTML_H4] = true;
+HEADING_ELEMENT_TYPES[HTML_H5] = true;
+HEADING_ELEMENT_TYPES[HTML_H6] = true;
+
 var INLINE_ELEMENTS_THAT_CAN_HAVE_CHILDREN = {
     "A": true,
     "B": true,
@@ -50,6 +91,13 @@ var INLINE_ELEMENTS_THAT_CAN_HAVE_CHILDREN = {
     "U": true,
     "SPAN": true,
 };
+
+var INLINE_ALLOWING_CHILDREN_TYPES = new Array(HTML_COUNT);
+INLINE_ALLOWING_CHILDREN_TYPES[HTML_A] = true;
+INLINE_ALLOWING_CHILDREN_TYPES[HTML_B] = true;
+INLINE_ALLOWING_CHILDREN_TYPES[HTML_I] = true;
+INLINE_ALLOWING_CHILDREN_TYPES[HTML_U] = true;
+INLINE_ALLOWING_CHILDREN_TYPES[HTML_SPAN] = true;
 
 var CONTAINER_ELEMENTS_ALLOWING_CONTENT = {
     "BODY": true,
@@ -61,6 +109,16 @@ var CONTAINER_ELEMENTS_ALLOWING_CONTENT = {
     "FIGCAPTION": true,
     "NAV": true,
 };
+
+var CONTAINERS_ALLOWING_CONTENT_TYPES = new Array(HTML_COUNT);
+CONTAINERS_ALLOWING_CONTENT_TYPES[HTML_BODY] = true;
+CONTAINERS_ALLOWING_CONTENT_TYPES[HTML_LI] = true;
+CONTAINERS_ALLOWING_CONTENT_TYPES[HTML_CAPTION] = true;
+CONTAINERS_ALLOWING_CONTENT_TYPES[HTML_TH] = true;
+CONTAINERS_ALLOWING_CONTENT_TYPES[HTML_TD] = true;
+CONTAINERS_ALLOWING_CONTENT_TYPES[HTML_FIGURE] = true;
+CONTAINERS_ALLOWING_CONTENT_TYPES[HTML_FIGCAPTION] = true;
+CONTAINERS_ALLOWING_CONTENT_TYPES[HTML_NAV] = true;
 
 var Keys = {
     HEADING_NUMBER: "uxwrite-heading-number",
@@ -96,27 +154,27 @@ var OPAQUE_NODE_CLASSES = {
 
 function isContainerNode(node)
 {
-    return CONTAINER_ELEMENTS[DOM_upperName(node)];
+    return CONTAINER_ELEMENT_TYPES[node._type];
 }
 
 function isParagraphNode(node)
 {
-    return PARAGRAPH_ELEMENTS[DOM_upperName(node)];
+    return PARAGRAPH_ELEMENT_TYPES[node._type];
 }
 
 function isHeadingNode(node)
 {
-    return HEADING_ELEMENTS[DOM_upperName(node)];
+    return HEADING_ELEMENT_TYPES[node._type];
 }
 
 function isTableCaptionNode(node)
 {
-    return (DOM_upperName(node) == "CAPTION");
+    return (node._type == HTML_CAPTION);
 }
 
 function isFigureCaptionNode(node)
 {
-    return (DOM_upperName(node) == "FIGCAPTION");
+    return (node._type == HTML_FIGCAPTION);
 }
 
 function isOutlineItemTitleNode(node)
@@ -136,40 +194,41 @@ function isInlineNode(node)
 
 function isListNode(node)
 {
-    return ((DOM_upperName(node) == "UL") || (DOM_upperName(node) == "OL"));
+    var type = node._type;
+    return ((type == HTML_UL) || (type == HTML_OL));
 }
 
 function isListItemNode(node)
 {
-    return (DOM_upperName(node) == "LI");
+    return (node._type == HTML_LI);
 }
 
 function isTableNode(node)
 {
-    return (DOM_upperName(node) == "TABLE");
+    return (node._type == HTML_TABLE);
 }
 
 function isTableCell(node)
 {
-    return ((DOM_upperName(node) == "TD") ||
-            (DOM_upperName(node) == "TH"));
+    var type = node._type;
+    return ((type == HTML_TD) || (type == HTML_TH));
 }
 
 function isFigureNode(node)
 {
-    return (DOM_upperName(node) == "FIGURE");
+    return (node._type == HTML_FIGURE);
 }
 
 function isRefNode(node)
 {
-    return ((DOM_upperName(node) == "A") &&
+    return ((node._type == HTML_A) &&
             node.hasAttribute("href") &&
             node.getAttribute("href").charAt(0) == "#");
 }
 
 function isImageNode(node)
 {
-    return (DOM_upperName(node) == "IMG");
+    return (node._type == HTML_IMG);
 }
 
 function isTextNode(node)
@@ -183,7 +242,7 @@ function isItemNumber(node)
         return isItemNumber(node.parentNode);
     }
     else if (node.nodeType == Node.ELEMENT_NODE) {
-        if ((DOM_upperName(node) == "SPAN") && node.hasAttribute("class")) {
+        if ((node._type == HTML_SPAN) && node.hasAttribute("class")) {
             return ITEM_NUMBER_CLASSES[node.getAttribute("class")];
         }
     }
@@ -201,7 +260,7 @@ function isOpaqueNode(node)
         if (node.hasAttribute("class") && OPAQUE_NODE_CLASSES[node.getAttribute("class")]) {
             return true;
         }
-        else if ((DOM_upperName(node) == "A") && node.hasAttribute("href")) {
+        else if ((node._type == HTML_A) && node.hasAttribute("href")) {
             return true;
         }
         else if (isImageNode(node)) {
@@ -213,7 +272,7 @@ function isOpaqueNode(node)
 
 function isAutoCorrectNode(node)
 {
-    return ((DOM_upperName(node) == "SPAN") &&
+    return ((node._type == HTML_SPAN) &&
             (node.getAttribute("class") == Keys.AUTOCORRECT_CLASS));
 }
 
@@ -225,7 +284,7 @@ function isSelectionHighlight(node)
 
 function isTOCNode(node)
 {
-    if (DOM_upperName(node) == "NAV") {
+    if (node._type == HTML_NAV) {
         var cls = node.getAttribute("class");
         if ((cls == Keys.SECTION_TOC) ||
             (cls == Keys.FIGURE_TOC) ||
@@ -260,5 +319,5 @@ function isContentLeafNode(node)
 
 function isAbstractSpan(node)
 {
-    return ((DOM_upperName(node) == "SPAN") && node.hasAttribute(Keys.ABSTRACT_ELEMENT));
+    return ((node._type == HTML_SPAN) && node.hasAttribute(Keys.ABSTRACT_ELEMENT));
 }
