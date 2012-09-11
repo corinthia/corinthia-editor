99250c55221e959afd27a248c2b7fa69fdace3c7
Peter Kelly <peter@uxproductivity.com>
2012-09-11 09:41:28 +0000

    Moved some obsolete code out of the javascript directory

--- src/docx/elements/DocxTableWidth.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2011-2012 UX Productivity Pty Ltd. All rights reserved.
-
-// Represents a tblW child of a tblPr element, or a tcW child of a tcPr element
-function DocxTableWidth(element)
-{
-    this.w = parseInt(element.getAttributeNS(WORD_NAMESPACE,"w"));
-    this.type = element.getAttributeNS(WORD_NAMESPACE,"type");
-}
--- src/latex/genlatex.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo "Enter latex document:"
-cat > document.tex
-pdflatex document.tex || exit 1
-pdflatex document.tex || exit 1
-pdflatex document.tex || exit 1
-open document.pdf
