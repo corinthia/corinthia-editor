69ca16c910a6ab6727a6a102a6c3cc18a5bf71e8
Peter Kelly <peter@uxproductivity.com>
2014-04-07 21:27:11 +0700

    Display cursor when in empty footnote or endnote

--- src/Position.js
+++ src/Position.js
@@ -675,6 +675,27 @@ var Position_atPoint;
                  height: rect.height };
     }
 
+    function zeroWidthMidRect(rect)
+    {
+        var mid = rect.left + rect.width/2;
+        return { left: mid,
+                 right: mid, // 0 width
+                 top: rect.top,
+                 bottom: rect.bottom,
+                 width: 0,
+                 height: rect.height };
+    }
+
+    var findNoteContainingPos = trace(function _findNoteContainingPos(pos)
+    {
+        var node = Position_closestActualNode(pos);
+        for (; node != null; node = node.parentNode) {
+            if (isNoteNode(node))
+                return node;
+        }
+        return null;
+    });
+
     var exactRectAtPos = trace(function exactRectAtPos(pos)
     {
         var node = pos.node;
@@ -737,6 +758,10 @@ var Position_atPoint;
         rect = exactRectAtPos(pos);
         if (rect != null)
             return rect;
+
+        var noteNode = findNoteContainingPos(pos);
+        if ((noteNode != null) && !nodeHasContent(noteNode)) // In empty footnote or endnote
+            return zeroWidthMidRect(noteNode.getBoundingClientRect());
 
         var paragraph = Text_findParagraphBoundaries(pos);
 
