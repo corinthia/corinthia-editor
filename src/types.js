0115a425ae93458b03259ca8e9f00566b4422a84
Peter Kelly <peter@uxproductivity.com>
2014-04-26 00:54:39 +0700

    splitAroundSelection: don't split footnotes

--- src/types.js
+++ src/types.js
@@ -123,6 +123,11 @@ function isBlockNode(node)
     return BLOCK_ELEMENTS[node._type];
 }
 
+function isBlockOrNoteNode(node)
+{
+    return BLOCK_ELEMENTS[node._type] || isNoteNode(node);
+}
+
 function isInlineNode(node)
 {
     return INLINE_ELEMENTS[node._type];
